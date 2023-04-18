32c0762a58bc070ad0940520fc5db1fdf16b67b4
kevin-astrafy <kevin@astrafy.io>
2023-04-18 07:58:45 +0200

    add providers names in outputs

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "pool_id" {
   value = google_iam_workload_identity_pool.this.workload_identity_pool_id
 }
 
-output "workload_identity_providers_ids" {
-  value = { for k,v in google_iam_workload_identity_pool_provider.this: k => { id = v.id}}
+output "workload_identity_providers" {
+  value = { for k,v in google_iam_workload_identity_pool_provider.this: k => { id = v.id, name = v.name}}
 }
